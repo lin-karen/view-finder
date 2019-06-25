c3d5b3fca7ca0d1c2a5e74ce1d21c0772d0acb15
Apple <apple@Apples-MacBook-Pro-63.local>
2019-06-25 14:06:18 -0400

    iteration 2

--- ViewFinder/AddPhotosViewController.swift
+++ ViewFinder/AddPhotosViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class AddPhotosViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
     var imagePicker = UIImagePickerController()
 
+    @IBOutlet weak var captionText: UITextField!
     @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
@@ -21,17 +21,30 @@ class AddPhotosViewController: UIViewController, UIImagePickerControllerDelegate
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func photoLibraryTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
     @IBAction func cameraTapped(_ sender: Any) {
         imagePicker.sourceType = .camera
         present(imagePicker, animated: true, completion: nil)
     }
 
-    @IBAction func photoLibraryTapped(_ sender: Any) {
-        imagePicker.sourceType = .photoLibrary
-        present(imagePicker, animated: true, completion: nil)
-    }
+    @IBAction func savePhotoTapped(_ sender: UIButton) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            let photoToSave = Photos(entity: Photos.entity(), insertInto: context)
+            photoToSave.caption = captionText.text
+        
+        if let userImage = imageView.image {
+            if let userImageData = userImage.pngData() {
+                photoToSave.imageData = userImageData
+            }
+        }
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+            navigationController?.popViewController(animated: true)
+        }
 
-    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
 
         if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = selectedImage
@@ -51,4 +64,5 @@ class AddPhotosViewController: UIViewController, UIImagePickerControllerDelegate
     }
     */
 
+}
 }
